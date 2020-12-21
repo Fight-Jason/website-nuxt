93db575b7c2279a4594120963ab9778e765e281b
Fight_Jason <18665308713@163.com>
2020-12-21 11:29:19 +0800

    init project

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul>
+      首页
+      <li>
+        <nuxt-link :to="{name:'index-pc-home'}">PCHOME</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link :to="{name:'index-m-home'}">MHOME</nuxt-link>
+      </li>
+    </ul>
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+	const routeMap = {
+		'index': 'index',
+		'/pc/home': '/m/home',
+		'/pc/about': '/m/about'
+	}
+
+	const map = new Map()
+  
+	for(let i in routeMap) {
+		map.set(i, routeMap[i])
+		map.set(routeMap[i], i)
+	}
+
+	export default {
+		asyncData({ store, redirect, route }) {
+			const { path } = route
+			const { isMobile } = store.state.global
+			if(path.includes('/pc/') && isMobile) {
+				redirect(map.get(path))
+			}
+			if(path.includes('/m/') && !isMobile) {
+				redirect(map.get(path))
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
